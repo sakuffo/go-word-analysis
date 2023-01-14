57bb17f17fe93458e3f977b4e012edc58b0cd712
Stephen Akuffo <sa.akuffo@gmail.com>
2023-01-14 17:47:03 -0500

    finished word count analysis

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -29,12 +30,18 @@ type Dataset struct {
 	reviews  []Review
 }
 
-func (dataset *Dataset) read_json_file() {
+func (dataset *Dataset) empty() bool {
+	if len(dataset.reviews) == 0 {
+		return true
+	}
+	return false
+}
+
+func (dataset *Dataset) read_json_file() (bool, error) {
 	content, err := os.Open(dataset.filepath)
 	if err != nil {
-		log.Fatal(err)
+		return true, err
 	}
-
 	defer content.Close()
 
 	scanner := bufio.NewScanner(content)
@@ -44,11 +51,11 @@ func (dataset *Dataset) read_json_file() {
 		var review Review
 		err := json.Unmarshal([]byte(scanner.Text()), &review)
 		if err != nil {
-			log.Fatal(err)
+			return true, err
 		}
-
 		dataset.reviews = append(dataset.reviews, review)
 	}
+	return false, nil
 }
 
 func read_json_file(filepath string) []Review {
@@ -78,10 +85,15 @@ func read_json_file(filepath string) []Review {
 	return reviews
 }
 
-func (dataset *Dataset) tokenize() {
+func (dataset *Dataset) tokenize() (bool, error) {
+	if dataset.empty() {
+		return true, errors.New("Dataset is empty. Please read the json file first.")
+	}
+
 	for i := range dataset.reviews {
 		dataset.reviews[i].Tokens = tokenize(dataset.reviews[i].ReviewText)
 	}
+	return false, nil
 }
 
 func tokenize(text string) []string {
@@ -111,16 +123,27 @@ func count_words(words []string) map[string]int {
 	return word_count
 }
 
-func (dataset *Dataset) count_words() {
+func (dataset *Dataset) count_words() (bool, error) {
+	if dataset.empty() {
+		return true, errors.New("Dataset is empty. Please read the json file first.")
+	}
+
 	for i := range dataset.reviews {
+		if len(dataset.reviews[i].Tokens) == 0 {
+			dataset.reviews[i].Tokens = tokenize(dataset.reviews[i].ReviewText)
+		}
 		dataset.reviews[i].WordCount = count_words(dataset.reviews[i].Tokens)
 	}
+	return false, nil
 }
 
 func main() {
 	dataset := Dataset{filepath: "./data/Digital_Music_5.json"}
 	dataset.read_json_file()
-	dataset.tokenize()
+	_, err := dataset.tokenize()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 	dataset.count_words()
 	for i := range dataset.reviews {
 		fmt.Println(dataset.reviews[i].ReviewText)
